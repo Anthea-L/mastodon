267db45475389a8f34735189174766b1083bb2fe
Anthea-L <37652722+Anthea-L@users.noreply.github.com>
2021-11-25 00:49:16 -0500

    Update instance_serializer.rb

--- app/serializers/rest/instance_serializer.rb
+++ app/serializers/rest/instance_serializer.rb
@@ -5,12 +5,8 @@ class REST::InstanceSerializer < ActiveModel::Serializer
 
   attributes :uri, :title, :short_description, :description, :email,
              :version, :urls, :stats, :thumbnail,
-<<<<<<< HEAD
              :languages, :registrations, :approval_required, :max_toot_chars, :invites_enabled
-=======
-             :languages, :registrations, :approval_required, :invites_enabled,
              :configuration
->>>>>>> v3.4.3
 
   has_one :contact_account, serializer: REST::AccountSerializer
 
